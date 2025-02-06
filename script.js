9da412e68fb133343bbf7c596834052f109f2a11
Mayanka11y <mayankraj418890@gmail.com>
2025-02-06 12:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+    if (!s.trim()) {
+        return '';
+    }
+    
+    let trimmedStr = s.trim();
+    let spaceIndex = trimmedStr.indexOf(' ');
+    
+    return spaceIndex === -1 ? trimmedStr : trimmedStr.substring(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
